9c835767dd416c4721fd9eedd9e07fa2f09c2d37
pHorvat <61691661+pHorvat@users.noreply.github.com>
2023-06-18 16:10:30 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,9 +14,9 @@ def upload_and_detect():
             output_file = run_detection(file_path)
             output_path = os.path.join('static', output_file)
             
-            return render_template('result.html', image_path=output_path)
+            return render_template_string(result_template, image_path=output_path)
         
-    return render_template('home.html')
+    return render_template_string(index_template)
 
 def run_detection(image_path):
     # Run the detect.py script with the provided image path
@@ -26,6 +26,35 @@ def run_detection(image_path):
     # Extract the output file name from the detect.py output
     output_file = 'runs/detect/detect_su57/' + os.path.basename(image_path)
     return output_file
+
+index_template = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload and Detect</title>
+</head>
+<body>
+    <h1>Upload an Image</h1>
+    <form method="POST" action="/" enctype="multipart/form-data">
+        <input type="file" name="file" accept=".jpg,.jpeg,.png">
+        <input type="submit" value="Detect">
+    </form>
+</body>
+</html>
+'''
+
+result_template = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Detection Result</title>
+</head>
+<body>
+    <h1>Detection Result</h1>
+    <img src="{{ image_path }}" alt="Detected Image">
+</body>
+</html>
+'''
 
 if __name__ == '__main__':
     app.run(debug=True)
